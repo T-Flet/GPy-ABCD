28c596fea3aa858a7fed3ed3a9e0c94ebf77db4a
Dr-Lord <Dr-Lord@outlook.com>
2019-11-07 21:50:44 +0000

    Changed all copy and deepcopy instances in add and multiply functions to new object instantiations, both for uniformity and because copy would not create new non-base-type fields, modifying the original objects in the process

--- kernelExpressionOperations.py
+++ kernelExpressionOperations.py
@@ -1,5 +1,4 @@
 from kernelExpression import *
-from copy import copy
 
 
 def sortOutTypePair(k1, k2):
@@ -11,11 +10,17 @@ def sortOutTypePair(k1, k2):
         return {t1: k1, t2: k2}
 
 
+# NOTE:
+#   copy and deepcopy from copy would look very elegant when partenered with methods that return self in some cases below, BUT:
+#       not using copy because it would affect the original ks non-base-type field values
+#       not using deepcopy because it would create much more than the required fields
+
+
 def add(k1, k2, root = None): # Simple addition, NOT DISTRIBUTING OR SIMPLIFYING
     pair = sortOutTypePair(k1, k2)
     if len(pair) == 1:
         if isinstance(k1, SumKE):
-            return copy(k1).new_base(k2.base_terms).new_composite(k2.composite_terms).set_root(root)
+            return SumKE(k1.base_terms + k2.base_terms, k1.composite_terms + k2.composite_terms, root)
         elif isinstance(k1, ProductKE):
             return SumKE([], [k1, k2], root)
         elif isinstance(k1, ChangeKE):
@@ -25,15 +30,15 @@ def add(k1, k2, root = None): # Simple addition, NOT DISTRIBUTING OR SIMPLIFYING
     else:
         if str in pair.keys():
             if SumKE in pair.keys():
-                return copy(pair[SumKE]).new_base(pair[str]).set_root(root)
+                return SumKE(pair[SumKE].base_terms + Counter([pair[str]]), pair[SumKE].composite_terms, root)
             elif ProductKE in pair.keys():
                 return SumKE([pair[str]], [pair[ProductKE]], root)
             else: # elif ChangeKE in pair.keys():
                 return SumKE([pair[str]], [pair[ChangeKE]], root)
         elif pair.keys() == {SumKE, ProductKE}:
-            return copy(pair[SumKE]).new_composite(pair[ProductKE]).set_root(root)
+            return SumKE(pair[SumKE].base_terms, pair[SumKE].composite_terms + [pair[ProductKE]], root)
         elif pair.keys() == {SumKE, ChangeKE}:
-            return copy(pair[SumKE]).new_composite(pair[ChangeKE]).set_root(root)
+            return SumKE(pair[SumKE].base_terms, pair[SumKE].composite_terms + [pair[ChangeKE]], root)
         else: # elif pair.keys() == {ProductKE, ChangeKE}:
             return SumKE([], list(pair.values()), root)
 
@@ -44,7 +49,7 @@ def multiply(k1, k2, root = None): # Simple multiplication, NOT DISTRIBUTING OR 
         if isinstance(k1, SumKE):
             return ProductKE([], [k1, k2], root)
         elif isinstance(k1, ProductKE):
-            return copy(k1).new_base(k2.base_terms).new_composite(k2.composite_terms).set_root(root)
+            return ProductKE(k1.base_terms + k2.base_terms, k1.composite_terms + k2.composite_terms, root)
         elif isinstance(k1, ChangeKE):
             return ProductKE([], [k1, k2], root)
         else: # elif isinstance(k1, str):
@@ -54,12 +59,12 @@ def multiply(k1, k2, root = None): # Simple multiplication, NOT DISTRIBUTING OR 
             if SumKE in pair.keys():
                 return ProductKE([pair[str]], [pair[SumKE]], root)
             elif ProductKE in pair.keys():
-                return copy(pair[ProductKE]).new_base(pair[str]).set_root(root)
+                return ProductKE(pair[ProductKE].base_terms + Counter([pair[str]]), pair[ProductKE].composite_terms, root)
             else: # elif ChangeKE in pair.keys():
                 return ProductKE([pair[str]], [pair[ChangeKE]], root)
         elif pair.keys() == {SumKE, ProductKE}:
-            return copy(pair[ProductKE]).new_composite(pair[SumKE]).set_root(root)
+            return ProductKE(pair[ProductKE].base_terms, pair[ProductKE].composite_terms + [pair[SumKE]], root)
         elif pair.keys() == {SumKE, ChangeKE}:
             return ProductKE([], list(pair.values()), root)
         else: # elif pair.keys() == {ProductKE, ChangeKE}:
-            return copy(pair[ProductKE]).new_composite(pair[ChangeKE]).set_root(root)
+            return ProductKE(pair[ProductKE].base_terms, pair[ProductKE].composite_terms + [pair[ChangeKE]], root)
