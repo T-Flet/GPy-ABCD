60e1cd14775d63ec6a5bb5dc058ae0629dd9c542
Dr-Lord <Dr-Lord@outlook.com>
2019-11-09 14:09:07 +0000

    Removed root arguments from kernelExpressionOperations. Created the basic expression expansion infrastructure and two production rules to start with.

---
+++ grammar.py
@@ -0,0 +1,88 @@
+from Kernels.baseKernels import *
+from kernelExpression import *
+from kernelExpressionOperations import *
+from copy import deepcopy
+from itertools import chain
+
+
+base_kerns = frozenset(['WN', 'C', 'LIN', 'SE', 'PER'])
+# stationary_kerns = frozenset(['WN', 'C', 'SE', 'PER'])
+# addition_idempotent_kerns = frozenset(['WN', 'C'])
+# multiplication_idempotent_kerns = frozenset(['WN', 'C', 'SE'])
+# multiplication_zero_kerns = frozenset(['WN']) # UNLESS LIN!!!!!!! I.E. ZERO ONLY FOR STATIONARY KERNELS
+# multiplication_identity_kerns = frozenset(['C'])
+
+base_order = {'PER': 1, 'WN': 2, 'SE': 3, 'C': 4, 'LIN': 5}
+    # Then sort by: sorted(LIST, key=lambda SYM: baseOrder[SYM])
+
+
+
+
+def flatten(list_of_lists):
+    return list(chain.from_iterable(list_of_lists))
+
+def expand_node(k_expr, p_rules):
+    return flatten([pr(k_expr) for pr in p_rules])
+
+def expand(k_expr, p_rules):
+    return flatten([expand_node(kex, p_rules) for kex in k_expr.traverse()])
+
+
+## Add more simplifications, e.g. regarding having singletons or nested singletons as children
+## Add duplicate removal and already-tested-expression filter here; need to implmenet equality comparison (not overloading __eq__ though)
+
+
+def deep_apply(operator, S, *args): # Deepcopy the tree and connect the new node to the rest of the tree
+    copied_node = deepcopy(S)
+    new_node = operator(copied_node, *args).set_parent(copied_node.parent)
+    if copied_node.root == copied_node:
+        new_node.set_root(new_node)
+    else:
+        new_node.set_root(copied_node.root)
+        new_node.parent.reassign_child(copied_node, new_node)
+    return new_node
+
+
+def plus_base(S): return [deep_apply(add, S, B) for B in base_kerns]
+def times_base(S): return [deep_apply(multiply, S, B) for B in base_kerns]
+# def replace_base(S): return [swap_base(S, B) for B in base_kerns]
+production_rules = { # IS A LIST BETTER BECAUSE KEYS ARE NEVER USED?
+    'plus_base': plus_base,
+    'times_base': times_base,
+    # 'replace_base': replace_base
+}
+
+
+# testExpr = ChangeKE('CP', ProductKE(['PER', 'C'], [SumKE(['WN', 'C', 'C'])]), ChangeKE('CW', 'SE', ProductKE(['WN', 'C'])))._initialise()
+# a = expand_node(testExpr, production_rules.values())
+# a = expand(testExpr, production_rules.values())
+# for e in a:
+#     print(e)
+
+
+
+
+
+# LHS:
+#   S: B | CP()  | CW() | ( .. )
+#   B: WN, C, LIN, SE, PER
+prod_rules_to_implement = [
+    ('S', 'S + B'),
+    ('S', 'S * B'),
+    ('B', 'B'),
+    ('S', 'CP(S, S)'),
+    ('S', 'CP(S, C)'),
+    ('S', 'CP(C, S)'),
+    ('S', 'CW(S, S)'),
+    ('S', 'CW(S, C)'),
+    ('S', 'CW(C, S)'),
+    ('S', 'S * (B + C)'),
+    ('S', 'B'),
+    ('S + S', 'S'),
+    ('S * S', 'S'),
+    ('S', '')
+]
+
+
+
+
--- kernelExpressionOperations.py
+++ kernelExpressionOperations.py
@@ -16,55 +16,55 @@ def sortOutTypePair(k1, k2):
 #       not using deepcopy because it would create much more than the required fields
 
 
-def add(k1, k2, root = None): # Simple addition, NOT DISTRIBUTING OR SIMPLIFYING
+def add(k1, k2): # Simple addition, NOT DISTRIBUTING OR SIMPLIFYING
     pair = sortOutTypePair(k1, k2)
     if len(pair) == 1:
         if isinstance(k1, SumKE):
-            return SumKE(k1.base_terms + k2.base_terms, k1.composite_terms + k2.composite_terms, root)
+            return SumKE(k1.base_terms + k2.base_terms, k1.composite_terms + k2.composite_terms)
         elif isinstance(k1, ProductKE):
-            return SumKE([], [k1, k2], root)
+            return SumKE([], [k1, k2])
         elif isinstance(k1, ChangeKE):
-            return SumKE([], [k1, k2], root)
+            return SumKE([], [k1, k2])
         else: # elif isinstance(k1, str):
-            return SumKE([k1, k2], [], root)
+            return SumKE([k1, k2], [])
     else:
         if str in pair.keys():
             if SumKE in pair.keys():
-                return SumKE(pair[SumKE].base_terms + Counter([pair[str]]), pair[SumKE].composite_terms, root)
+                return SumKE(pair[SumKE].base_terms + Counter([pair[str]]), pair[SumKE].composite_terms)
             elif ProductKE in pair.keys():
-                return SumKE([pair[str]], [pair[ProductKE]], root)
+                return SumKE([pair[str]], [pair[ProductKE]])
             else: # elif ChangeKE in pair.keys():
-                return SumKE([pair[str]], [pair[ChangeKE]], root)
+                return SumKE([pair[str]], [pair[ChangeKE]])
         elif pair.keys() == {SumKE, ProductKE}:
-            return SumKE(pair[SumKE].base_terms, pair[SumKE].composite_terms + [pair[ProductKE]], root)
+            return SumKE(pair[SumKE].base_terms, pair[SumKE].composite_terms + [pair[ProductKE]])
         elif pair.keys() == {SumKE, ChangeKE}:
-            return SumKE(pair[SumKE].base_terms, pair[SumKE].composite_terms + [pair[ChangeKE]], root)
+            return SumKE(pair[SumKE].base_terms, pair[SumKE].composite_terms + [pair[ChangeKE]])
         else: # elif pair.keys() == {ProductKE, ChangeKE}:
-            return SumKE([], list(pair.values()), root)
+            return SumKE([], list(pair.values()))
 
 
-def multiply(k1, k2, root = None): # Simple multiplication, NOT DISTRIBUTING OR SIMPLIFYING
+def multiply(k1, k2): # Simple multiplication, NOT DISTRIBUTING OR SIMPLIFYING
     pair = sortOutTypePair(k1, k2)
     if len(pair) == 1:
         if isinstance(k1, SumKE):
-            return ProductKE([], [k1, k2], root)
+            return ProductKE([], [k1, k2])
         elif isinstance(k1, ProductKE):
-            return ProductKE(k1.base_terms + k2.base_terms, k1.composite_terms + k2.composite_terms, root)
+            return ProductKE(k1.base_terms + k2.base_terms, k1.composite_terms + k2.composite_terms)
         elif isinstance(k1, ChangeKE):
-            return ProductKE([], [k1, k2], root)
+            return ProductKE([], [k1, k2])
         else: # elif isinstance(k1, str):
-            return ProductKE([k1, k2], [], root)
+            return ProductKE([k1, k2], [])
     else:
         if str in pair.keys():
             if SumKE in pair.keys():
-                return ProductKE([pair[str]], [pair[SumKE]], root)
+                return ProductKE([pair[str]], [pair[SumKE]])
             elif ProductKE in pair.keys():
-                return ProductKE(pair[ProductKE].base_terms + Counter([pair[str]]), pair[ProductKE].composite_terms, root)
+                return ProductKE(pair[ProductKE].base_terms + Counter([pair[str]]), pair[ProductKE].composite_terms)
             else: # elif ChangeKE in pair.keys():
-                return ProductKE([pair[str]], [pair[ChangeKE]], root)
+                return ProductKE([pair[str]], [pair[ChangeKE]])
         elif pair.keys() == {SumKE, ProductKE}:
-            return ProductKE(pair[ProductKE].base_terms, pair[ProductKE].composite_terms + [pair[SumKE]], root)
+            return ProductKE(pair[ProductKE].base_terms, pair[ProductKE].composite_terms + [pair[SumKE]])
         elif pair.keys() == {SumKE, ChangeKE}:
-            return ProductKE([], list(pair.values()), root)
+            return ProductKE([], list(pair.values()))
         else: # elif pair.keys() == {ProductKE, ChangeKE}:
-            return ProductKE(pair[ProductKE].base_terms, pair[ProductKE].composite_terms + [pair[ChangeKE]], root)
+            return ProductKE(pair[ProductKE].base_terms, pair[ProductKE].composite_terms + [pair[ChangeKE]])
